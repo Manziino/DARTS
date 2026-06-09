54d1bb1b2aea9d978dfa8282d74b56971ad728ac
Manziino <luca38600@gmail.com>
2026-06-09 14:35:28 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,31 @@
-const CACHE_NAME = 'darts-v7';
-const ASSETS = [
-  'index.html',
-  'manifest.json'
-];
+const CACHE_NAME = 'darts-v8-final';
+const ASSETS = ['index.html', 'manifest.json'];
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+// C'EST CETTE PARTIE QUI DÉBLOQUE LES MISES À JOUR
+self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
 
+// STRATÉGIE "NETWORK FIRST"
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
     })
   );
 });
