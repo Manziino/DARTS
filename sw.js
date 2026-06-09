e477a2595fb0b8778f2079f11e9ea69b1b569fa4
Manziino <luca38600@gmail.com>
2026-06-09 03:16:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,15 @@
-const CACHE_NAME = 'darts-v6';
-const ASSETS = [
-  'index.html',
-  'manifest.json'
-];
+const CACHE_NAME = 'darts-v7';
+const ASSETS = ['index.html', 'manifest.json'];
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map(k => caches.delete(k)))));
+});
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
